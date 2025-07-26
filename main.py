d2b3dec5427e79158a78b27ba7700666a365e0c9
Keeroh-jpg <samuel.baugh97@gmail.com>
2025-07-26 10:22:32 +0100

    adding --verbose functionality

--- main.py
+++ main.py
@@ -2,23 +2,32 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+user_prompt = sys.argv[1]
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])
+            ]
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print("Error! Incorrect input. Usage: uv run main.py <prompt>")
     sys.exit(1)
 
 
-prompt = sys.argv[1]
-response = client.models.generate_content(model='gemini-2.0-flash-001', contents=f"{prompt}")
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+
+if "--verbose" in sys.argv:
+    print(f"User prompt: {user_prompt}")
+    print(f"Response: {response.text}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+else:
+    print(response.text)
 
 
 
