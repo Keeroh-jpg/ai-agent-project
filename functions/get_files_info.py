b533be3930ba9fd3042698e90a6db111dfdc33c2
Keeroh-jpg <samuel.baugh97@gmail.com>
2025-07-26 16:23:31 +0100

    get_files_info function created

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,16 +3,20 @@ import os
 
 def get_files_info(working_directory, directory="."):
     full_path = os.path.join(working_directory, directory)
-    if os.path.abspath(directory) not in working_directory:
+    full_path = os.path.normpath(full_path)
+    
+    if full_path.startswith(working_directory) is False:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory.'
-    if not os.path.isdir(directory):
+    if not os.path.isdir(full_path):
         return f'Error: "{directory}" is not a directory.'
     directory_contents = os.listdir(full_path)
     outputs = []
     for content in directory_contents:
-        if os.path.isfile(content):
-            outputs.append(f"- {content}: file_size={os.path.getsize(content)}, is_dir=False")
-        elif os.path.isdir(content):
-            outputs.append(f"- {content}: file_size={os.path.getsize(content)}, is_dir=True")
+        content_full_path = os.path.join(full_path, content)
+        
+        if os.path.isfile(content_full_path):
+            outputs.append(f"- {content}: file_size={os.path.getsize(content_full_path)} bytes, is_dir=False")
+        elif os.path.isdir(content_full_path):
+            outputs.append(f"- {content}: file_size={os.path.getsize(content_full_path)} bytes, is_dir=True")
 
     return "\n".join(outputs)
